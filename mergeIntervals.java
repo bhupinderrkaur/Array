ea12ef2980019c6b0401b3571bcdb2b5fe463edd
Bhupinder Kaur <99190152+bhupinderrkaur@users.noreply.github.com>
2022-04-06 13:35:02 +0530

    Create mergeIntervals.java

---
+++ mergeIntervals.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
+        //if list is empty
+        if(intervals == null || intervals.size()==0) {
+        intervals=new ArrayList();
+        intervals.add(newInterval);
+        return intervals;
+    }
+        Interval toInsert = newInterval;
+        int i=0;
+        while(i<intervals.size()) {
+	    Interval current=intervals.get(i);
+	    if(current.end < toInsert.start) {
+		i++;
+		continue;
+	    }
+        else if(current.start>toInsert.end) {
+		intervals.add(i,toInsert);
+		break;
+	    }
+        else {
+		toInsert.start=Math.min(current.start, toInsert.start);
+		toInsert.end=Math.max(current.end, toInsert.end);
+		intervals.remove(i);
+	    }
+}
+if(i == intervals.size()) {
+	intervals.add(toInsert);
+}
+return intervals;
+}
+}
